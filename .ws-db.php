fed9db550632ba2b6b80419e8ba4a1e6b306135d
Ed Baker <edwbaker@gmail.com>
2023-07-14 10:07:04 +0000

    Add project boilerplate.

--- .ws-db.php
+++ .ws-db.php
@@ -1,1 +1,9 @@
+<?php
+
+# WildlifeSystems
+#
+# This script is part of the WildlifeSystems project. For further information
+# please refer to https://docs.wildlife.systems, or for more information on
+# the project itself, please refer to https://wildlife.systems.
+
 $db = new mysqli("localhost","user","password","database");
